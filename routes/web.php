e1e957dff1bb8433fd44e4a6f79589b7ef90a976
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2025-03-04 01:55:30 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,12 @@ use Botble\Ipara\Http\Controllers\IparaController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware(['core','web'])->group(function () {
+Route::middleware(['core'])->group(function () {
     Route::post('payment/ipara/webhook', [IparaController::class, 'webhook'])
         ->name('payments.ipara.webhook');
 
     Route::any('payment/ipara/callback', [IparaController::class, 'callback'])
-        ->name('payments.ipara.callback')
-        ->withoutMiddleware('web');
+        ->name('payments.ipara.callback');
 
     Route::post('payment/ipara/process', [IparaController::class, 'process'])
         ->name('payments.ipara.process');
